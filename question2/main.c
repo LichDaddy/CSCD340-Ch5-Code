c2a9a90515cb8f96e3b1f8b33899370509400ec3
Michael Sinclair II <140662300+LichDaddy@users.noreply.github.com>
2024-04-16 00:39:25 +0000

    Redid Question2

--- question2/main.c
+++ question2/main.c
@@ -19,12 +19,13 @@ int main(int argc, char *argv[]) {
     // child: redirect standard output to a file
     else if (rc == 0) {
         close(STDOUT_FILENO);
-        open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+        open("./child.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
 
         // we are creating a pointer of char because we can use this 
         // to pass in strings efficiently to the execvp function
         char *myargs[3];
-        myargs[0] = strdup("wc"); // this is like piping in the command "wc" into the terminal
+        // open the file to write to and to see the output of the hexdump command if it will open the file twice
+        myargs[0] = strdup("hexdump"); // this is like piping in the command "wc" into the terminal
         myargs[1] = strdup("loren.txt"); // the file we want to count
         myargs[2] = NULL; // placeholder for the end of the array
         execvp(myargs[0], myargs); // this is like running the command "wc p4.c" in the terminal
@@ -36,14 +37,17 @@ int main(int argc, char *argv[]) {
     else {
         
         close(STDOUT_FILENO);
-        open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);      
+        // open the file to write to and to see the output of the wc command if it will open the file twice
+        open("./parent.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);      
 
         char *myargs[3];
         // we are diffrerentiating the two commands by using the -l flag in the wc command
-        myargs[0] = strdup("wc -l"); // this is like piping in the command "wc -l" into the terminal
+        myargs[0] = strdup("wc"); // this is like piping in the command "wc -l" into the terminal
         myargs[1] = strdup("loren.txt"); // the file we want to count
         myargs[2] = NULL; // placeholder for the end of the array
         execvp(myargs[0], myargs); // this is like running the command "wc p4.c" in the terminal  
+
+        fprintf(stderr, "in the parent\n");        
 
     } // end of else statement
 
