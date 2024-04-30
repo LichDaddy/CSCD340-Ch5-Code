c578e496dc58c39b022808f0df847baaabb30ff5
Michael Sinclair II <140662300+LichDaddy@users.noreply.github.com>
2024-04-30 05:13:18 +0000

    fixed argv to argc

--- ch13/memory-user.c
+++ ch13/memory-user.c
@@ -3,12 +3,25 @@
 #include <unistd.h>
 
 int main(int argc, char *argv []) {
-    if (argv < 2){
+    if (argc < 2){
         fprintf(stderr, "Not enough arguments\n");
     } // end of if statement
 
     // argv[0] is the name of the program
     // argv[1] is the first argument passed to the program
     int size = atoi(argv[1]);
+
+    // make the array size 1MB times the size given
+    int arraySize = size * 1024 * 1024;
+
+    // allocate that size
+    int *array = (int *) malloc(arraySize);
+
+    // will run because always true
+    while (0 == 0){
+        for (int i = 0; i < arraySize; i++){
+            array[i] = 0;
+        } // end of for loop
+    } // end of while loop
 
 } // end of main function
